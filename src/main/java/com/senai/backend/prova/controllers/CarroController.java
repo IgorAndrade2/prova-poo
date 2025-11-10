7c221ed2070a1f60777e0917834b2d14912a4860
IgorAndrade2 <igor.a.ferreira@aluno.senai.br>
2025-11-10 16:47:58 -0300

    inserçao do crud em carros

--- src/main/java/com/senai/backend/prova/controllers/CarroController.java
+++ src/main/java/com/senai/backend/prova/controllers/CarroController.java
@@ -1,7 +1,12 @@
 package com.senai.backend.prova.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,4 +30,29 @@ public class CarroController {
     public Carro salvar(@RequestBody Carro carro){
         return carroService.salvar(carro);
     }
+
+     // ✅ Buscar por ID (GET)
+    @GetMapping("/{id}")
+    public Optional<Carro> buscarPorId(@PathVariable Long id) {
+        return carroService.buscarPorId(id);
+    }
+
+    // ✅ Listar todos (GET)
+    @GetMapping("/listar")
+    public List<Carro> listarTodos() {
+        return carroService.listarTodos();
+    }
+
+    // ✅ Contar registros (GET)
+    @GetMapping("/contar")
+    public Long contar() {
+        return carroService.contar();
+    }
+
+    // ✅ Remover por ID (DELETE)
+    @DeleteMapping("/remover/{id}")
+    public void remover(@PathVariable Long id) {
+        carroService.remover(id);
+    }
+
 }
--- src/main/java/com/senai/backend/prova/services/CarroService.java
+++ src/main/java/com/senai/backend/prova/services/CarroService.java
@@ -1,5 +1,8 @@
 package com.senai.backend.prova.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +15,21 @@ public class CarroService {
     private CarroRepository carroRepository;
     public Carro salvar(Carro carro){
         return carroRepository.save(carro);
+    }
+
+     public Optional<Carro> buscarPorId(Long id) {
+        return carroRepository.findById(id);
+    }
+
+    public List<Carro> listarTodos() {
+        return carroRepository.findAll();
+    }
+
+    public Long contar() {
+        return carroRepository.count();
+    }
+
+    public void remover(Long id) {
+        carroRepository.deleteById(id);
     }
 }
